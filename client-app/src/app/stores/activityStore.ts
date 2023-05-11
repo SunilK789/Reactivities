12f109223b88a6914f569a3164cca6c74a5cb273
Sunil Kumar <sunilpalarya@gmail.com>
2023-05-11 14:54:13 +0530

    added popup for attendee

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -174,4 +174,19 @@ export default class ActivityStore {
 		}
 	}
 
+	cancelActivityToggle = async () =>{
+			this.loading = true;
+			try {
+				await agent.Activities.attend(this.selectedActivity!.id);
+				runInAction(()=>{
+					this.selectedActivity!.isCancelled = !this.selectedActivity?.isCancelled;
+					this.activiyRegistory.set(this.selectedActivity!.id, this.selectedActivity!);
+				});
+			} catch (error) {
+				console.log(error);
+			}finally{
+				runInAction(()=> this.loading = false);
+			}
+	}
+
 }
